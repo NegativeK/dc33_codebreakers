40c11d59b17f3faa3b9ffcb5960d6f4af87a3e0f
NegativeK <github@negk.com>
2025-08-08 21:18:15 -0700

    Simple CLI for testing guesses

---
+++ 02/curl.sh
@@ -0,0 +1,6 @@
+curl 'https://code-two.replit.app/api/challenge/submit' \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -H 'Cookie: connect.sid=s%3AYKDcyaksSHOxvIiiFBsjB.mfZAacFprreQ2nooxaMUmVm1XkyGFqoFugwDEhrGMOc' \
+  --data-raw '{"code": "undergroundnet.ru"}'
+# --data-raw '{"code":"testtt"}'
